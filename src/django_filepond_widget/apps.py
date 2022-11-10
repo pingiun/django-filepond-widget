45f8052d8bd975cf0533fb5f251ea57da01d654a
Jelle Besseling <jelle@pingiun.com>
2022-11-10 14:21:30 +0100

    Fix app name

--- src/django_filepond_widget/apps.py
+++ src/django_filepond_widget/apps.py
@@ -5,5 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class FilepondWidgetConfig(AppConfig):
     """AppConfig class for Filepond Widget app."""
 
-    name = "filepond_widget"
-    verbose_name = _("Filepond Wisdget")
+    name = "django_filepond_widget"
+    verbose_name = _("Filepond Widget")
